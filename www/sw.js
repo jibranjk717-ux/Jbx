7d755625f35e6a177a4d5303e35904cfb339c14c
jibranjk717-ux <jibran.jk717@gmail.com>
2026-08-26 21:47:41 +0000

    Refresh ApeX cache on every update

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'apex-v2';
+const CACHE = 'apex-v3';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
@@ -6,7 +6,9 @@ self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('apex-') && key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
 self.addEventListener('fetch', event => {
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
 });
